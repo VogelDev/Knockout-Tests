00d240e53b82df3eacb90a3b31506454434c263a
VogelDev <rvogel@mc3.edu>
2017-05-29 20:38:18 -0400

    client side adding now works

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,49 @@
 $(document).ready(function() {
-	$.getJSON("./data/data.php", function(data) {
-		var themes = ["blue", "orange", "OrangeRed", "Peru", "olive", "MediumVioletRed"]
-		for (var i = 0; i < data.length; i++) {
-			data[i].theme = themes[Math.floor(Math.random() * themes.length)];
-		}
-		//viewModel.people(data.people);
-		viewModel.people(data);
-	});
+  $.getJSON("./data/data.php", function(data) {
+    var themes = ["blue", "orange", "OrangeRed", "Peru", "olive", "MediumVioletRed"]
+    for (var i = 0; i < data.length; i++) {
+      data[i].theme = themes[Math.floor(Math.random() * themes.length)];
+    }
+    //viewModel.people(data.people);
+    viewModel.people(data);
+  });
 });
 
 var ContactListModel = function() {
+	var self = this;
   this.personToAdd = ko.observable("");
   this.people = ko.observableArray([]);
   this.selectedItems = ko.observableArray("");
 
-  this.addPerson = function() {
-    if (this.personToAdd() !== "") {
-      this.people.push(this.personToAdd());
+  this.peopleToAdd = ko.observableArray("");
+  this.peopleToRemove = ko.observableArray("");
+
+  this.addContact = function() {
+
+    var person = {
+			ID:					self.people().length + 1,
+      FIRST_NAME: $("#firstNameInput").val(),
+      LAST_NAME: 	$("#lastNameInput").val(),
+      TITLE: 			$("#titleInput").val(),
+      PHONE: 			$("#phoneInput").val(),
+      EMAIL: 			$("#emailInput").val(),
+			theme:      "OrangeRed"
     }
-    this.personToAdd("");
+
+		this.people.push(person);
+
+		$("#firstNameInput").val(""),
+		$("#lastNameInput").val(""),
+		$("#titleInput").val(""),
+		$("#phoneInput").val(""),
+		$("#emailInput").val("")
+
   }
+
+	this.saveAll = function(){
+		$.post("data/save.php", person);
+	}
+
 }
 
 var viewModel = new ContactListModel();
